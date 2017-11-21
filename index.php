7367b2b6fccfa15b58869c93f4d0c798bdcd7b50
faridkhan786 <farid-786@outlook.com>
2017-11-21 20:17:30 +0000

    farid_work
    hotel_management_system

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h2>Welcome to my Website</h2>';
+?>
+<html>
+<body>
+<marquee>This text is moving</marquee>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,134 @@
+<html>
+<head lang="en">
+  <meta charset="UTF-8">
+<title>Home Page </title>
+<link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> 
+</head>
+<body>
+<nav>
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+
+
+
+<a class="navbar-brand" href="#">Hamza Royal Hotel</a>
+ </div>
+<a href="#">HOME</a>
+<a href="#">ABOUT</a>
+<a href="#">TEAM</a>
+<a href="gallery.php">GALLERY</a>
+<a href="#">CONTACTS</a>
+
+<ul class="nav navbar-nav navbar-right">
+<li class="dropdown">
+<a href="##############" class="dropdown-toggle" data-toggle="dropdown">Register <i class="fa fa-user-plus"> </i><span class="caret"></span></a>
+<ul class="dropdown-menu dropdown-lr animated flipInX" role="menu">
+<div class="col-lg-12">
+<div class="text-center">
+<h3><b>Register</b></h3></div>
+<form id="ajax-register-form" action="##############" method="post" role="form" autocomplete="off">
+<div class="form-group">
+<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+</div>
+<div class="form-group">
+<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+</div>
+<div class="form-group">
+<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+</div>
+<div class="form-group">
+<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+<div>
+<div class="form-group">
+<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+</div>
+  <select class="form-control" id="sel1" >
+    <option>Staff</option>
+    <option>Admin</option>
+  </select>
+</div>
+</div>
+
+<div class="form-group">
+<div class="row">
+<div class="col-xs-6 col-xs-offset-3">
+<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-info" value="Register Now">
+</div>
+</div>
+</div>
+<input type="hidden" class="hide" name="token" id="token" value="7c6f19960d63f53fcd05c3e0cbc434c0">
+</form>
+</div>
+</ul>
+</li>
+
+<li class="dropdown">
+<a href="############" class="dropdown-toggle" data-toggle="dropdown">Log In<i class="fa fa-user"> </i> <span class="caret"></span></a>
+<ul class="dropdown-menu dropdown-lr animated slideInRight" role="menu">
+<div class="col-lg-12">
+<div class="text-center">
+<h3><b>Log In</b></h3></div>
+<form id="ajax-login-form" action="##########" method="post" role="form" autocomplete="off">
+<div class="form-group">
+<label for="username">Username</label>
+<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" autocomplete="off">
+</div>
+
+<div class="form-group">
+<label for="password">Password</label>
+<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" autocomplete="off">
+</div>
+
+<div class="form-group">
+<div class="row">
+<div class="col-xs-7">
+<input type="checkbox" tabindex="3" name="remember" id="remember">
+<label for="remember"> Remember Me</label>
+</div>
+<div class="col-xs-5 pull-right">
+<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-success" value="Log In">
+</div>
+</div>
+</div>
+
+ <div class="form-group">
+ <div class="row">
+ <div class="col-lg-12">
+ <div class="text-center">
+ <a href="#########" tabindex="5" class="forgot-password">Forgot Password?</a>
+ </div>
+ </div>
+ </div>
+ </div>
+ <input type="hidden" class="hide" name="token" id="token" value="a465a2791ae0bae853cf4bf485dbe1b6">
+ </form>
+ </div>
+ </ul>
+ </li>
+ </ul>
+ </div>
+ </div>
+</nav>
+</ul>
+</div>
+</div>
+</nav>
+</nav>
+ <div class="sect sectOne"></div>
+ <div class="subSection"></div>
+ <div class="sect sectTwo"></div>
+ <div class="subSection"></div>
+ <div class="sect sectThree"></div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
